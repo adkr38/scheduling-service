3baa994a5b67f3866510c11b9739ea4b6ac4a6fe
adriankr38 <adrian58ago@gmail.com>
2023-05-06 20:19:32 +0100

    add(tsx): `Mini-nav` for secondary components

---
+++ frontend/src/components/ComponentNav.tsx
@@ -0,0 +1,40 @@
+// import React from "react";
+import { ReactNode, useContext } from "react";
+import React from "react";
+import * as bs from "react-icons/bs";
+import * as ai from "react-icons/ai";
+import * as gr from "react-icons/gr";
+import { ThemeContext } from "../context/ThemeContext";
+
+const ComponentNav = () => {
+  const lightButton =
+    "text-stone-600 font-raleway text-md border border-stone-900 bg-stone-50 rounded-md px-4 py-0.5 active:bg-stone-200";
+
+  const darkButton =
+    "text-stone-200 font-raleway text-md border border-stone-50 bg-stone-800 rounded-md px-4 py-0.5 active:bg-stone-600";
+
+  const lightIcon = "text-2xl text-stone-600";
+  const darkIcon = "text-2xl text-stone-200";
+
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
+
+  return (
+    <nav className="right-16 sm:mr-10 flex flex-row items-center gap-x-4 absolute">
+      <button className={theme == "light" ? lightIcon : darkIcon}>
+        <ai.AiOutlineHome></ai.AiOutlineHome>
+      </button>
+      <button
+        className={
+          theme == "light"
+            ? "text-xl text-stone-600 pl-1"
+            : "text-xl text-stone-200 pl-1"
+        }
+        onClick={toggleTheme}
+      >
+        <bs.BsMoon></bs.BsMoon>
+      </button>
+    </nav>
+  );
+};
+
+export default ComponentNav;
