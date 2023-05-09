7dfcb1a614c84694eb0a3f10fa8ce9a235961ce7
adriankr38 <adrian58ago@gmail.com>
2023-05-09 17:17:03 +0100

    fix(tsx): Remove Button tag on Link

--- frontend/src/components/ComponentNav.tsx
+++ frontend/src/components/ComponentNav.tsx
@@ -5,23 +5,17 @@ import { ThemeContext } from "../context/ThemeContext";
 import { Link } from "react-router-dom";
 
 const ComponentNav = () => {
-  const lightButton =
-    "text-stone-600 font-raleway text-md border border-stone-900 bg-stone-50 rounded-md px-4 py-0.5 active:bg-stone-200";
-
-  const darkButton =
-    "text-stone-200 font-raleway text-md border border-stone-50 bg-stone-800 rounded-md px-4 py-0.5 active:bg-stone-600";
-
   const lightIcon = "text-2xl text-stone-600";
   const darkIcon = "text-2xl text-stone-200";
 
   const { theme, toggleTheme } = React.useContext(ThemeContext);
 
   return (
-    <nav className="right-16 sm:mr-10 flex flex-row items-center gap-x-4 absolute">
+    <nav className="right-16 sm:mr-10 flex flex-row items-center justify-center gap-x-4 absolute">
       <Link to="/">
-        <button className={theme == "light" ? lightIcon : darkIcon}>
-          <ai.AiOutlineHome></ai.AiOutlineHome>
-        </button>
+        <ai.AiOutlineHome
+          className={theme == "light" ? lightIcon : darkIcon}
+        ></ai.AiOutlineHome>
       </Link>
       <button
         className={
