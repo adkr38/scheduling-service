5446df9a2e8c57e8bd8d45da664e73ef24128f66
adriankr38 <adrian58ago@gmail.com>
2023-05-21 13:01:20 +0100

    fix(java): lombok conflicts

--- backend/src/main/java/com/adkr38/app/models/Role.java
+++ backend/src/main/java/com/adkr38/app/models/Role.java
@@ -1,31 +1,19 @@
 package com.adkr38.app.models;
 import jakarta.persistence.*;
 import com.adkr38.app.enums.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private RoleType role;
 
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public RoleType getRole() {
-	return role;
-}
-public void setRole(RoleType role) {
-	this.role = role;
-}
-
-  public Role(RoleType role){
-    setRole(role);
-  }
-
-  public Role(){};
+  private String name;
 
 }
