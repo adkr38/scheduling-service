82d6f2cd48d0c3350bcdb6f2af1f6a84beeb853d
adriankr38 <adrian58ago@gmail.com>
2023-05-21 13:04:01 +0100

    add(java): `Optional<User>` & `User` find methods

--- backend/src/main/java/com/adkr38/app/repositories/UserRepository.java
+++ backend/src/main/java/com/adkr38/app/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.adkr38.app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import com.adkr38.app.models.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
+
+  Optional<User> findByUsername(String username);
+  User findOneByUsername(String username);
 
 }
