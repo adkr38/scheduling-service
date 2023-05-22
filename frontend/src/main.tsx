23bf3529c48459c25a9b1674b38380372e16d8c9
adriankr38 <adrian58ago@gmail.com>
2023-05-22 12:44:03 +0100

    add(tsx): `<LoginProvider />` wrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext";
+import { LoginProvider } from "./context/LoginContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <LoginProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LoginProvider>
   </React.StrictMode>
 );
